af090ab11738c87cd98f4b722a3e1ebd39a24f9b
mrsekut <k.cloudspider@gmail.com>
2019-08-17 03:23:28 +0900

    spacesC

--- src/others/parser/parsec.hs
+++ src/others/parser/parsec.hs
@@ -1,3 +1,10 @@
+module Parser
+    ( expr
+    , parseExpr
+    , Expr(..)
+    )
+where
+
 import           Text.Parsec
 import           Control.Applicative            ( (<$>)
                                                 , (*>)
@@ -6,33 +13,112 @@ import           Control.Applicative            ( (<$>)
                                                 )
 import           Text.Parsec.String
 import           Data.Char                      ( digitToInt )
+import           Data.Functor.Identity
 
-data Expr = Add Expr Expr | Mul Expr Expr | Nat Int deriving Show
 
--- expr ::= term ('+' expr | term)
+data Expr = Add Expr Expr       -- 1 + 2
+          | Sub Expr Expr       -- 1 - 2
+          | Mul Expr Expr       -- 1 * 2
+          | Div Expr Expr       -- 1 / 2
+          | Eq Expr Expr        -- 1 == 2
+          | Neq Expr Expr       -- 1 == 2
+          | Lt Expr Expr        -- 1 < 2
+          | Gt Expr Expr        -- 1 > 2
+          | Lte Expr Expr       -- 1 <= 2
+          | Gte Expr Expr       -- 1 >= 2
+          | Nat Int             -- 1,2,..
+            deriving Show
+
+-- TODO: Either Monad
+-- TODO: 右結合になっている
+-- expr ::= add
 expr :: Parser Expr
-expr = do
-    t <- term
-    (Add t <$> (char '+' *> expr)) <|> pure t
+expr = assign
 
 
--- term ::= factor ('*' term | factor)
+-- assigns ::= equality | equality "=" assign
+assign :: Parser Expr
+assign = equality
+
+
+-- equality ::= add | add ("==" relational | "!=" relatoinal)
+equality :: Parser Expr
+equality = do
+    r <- spaces *> relational
+    (Eq r <$> (spaces *> string "==" *> spaces *> equality))
+        <|> (Neq r <$> (spaces *> string "!=" *> spaces *> equality))
+        <|> pure r
+
+
+-- relational ::= add | add ("<" add | "<=" add | ">" add | ">=" add)
+relational :: Parser Expr
+relational = do
+    a <- spaces *> add
+    (do
+            spaces *> char '<'
+            (Lt a <$> (spaces *> relational))
+                <|> (Lte a <$> (char '=' *> spaces *> relational))
+        )
+        <|> (do
+                spaces *> char '>'
+                (Gt a <$> (spaces *> relational))
+                    <|> (Gte a <$> (char '=' *> spaces *> relational))
+            )
+        <|> pure a
+
+
+-- addMap = [('+', Add), ('-', Sub)]
+-- convertAddOp :: [(Char, Expr)] -> Char -> Maybe Expr
+-- convertAddOp addMap s = lookup s addMap
+
+spacesC :: Char -> Parser Expr -> ParsecT String () Identity Expr
+spacesC c f = spaces *> char c *> spaces *> f
+
+-- add ::= term | term ('+' add | "-" add)
+add :: Parser Expr
+add = do
+    t <- spaces *> term
+    (Add t <$> spacesC '+' add) <|> (Sub t <$> spacesC '-' add) <|> pure t
+
+
+-- term ::= unary | unary ('*' unary |'/' unary)
 term :: Parser Expr
 term = do
-    f <- factor
-    (Mul f <$> (char '*' *> term)) <|> pure f
+    u <- spaces *> unary
+    (Mul u <$> spacesC '*' term) <|> (Div u <$> spacesC '/' term) <|> pure u
+
+
+-- unary ::= factor | ('+' | '-') factor
+unary :: Parser Expr
+unary =
+    (spaces *> char '+' *> factor)
+        <|> Sub (Nat 0)
+        <$> (spacesC '-' unary)
+        <|> factor
 
 
 -- factor ::= '(' expr ')' | nat
 factor :: Parser Expr
-factor = (char '(' *> expr <* char ')') <|> nat
+factor =
+    (spaces *> char '(' *> spaces *> expr <* spaces <* char ')' <* spaces)
+        <|> spaces
+        *>  nat
+        <*  spaces
 
 
 -- nat ::= '0' | '1' | '2' | ...
 nat :: Parser Expr
-nat = Nat . digitToInt <$> oneOf ['0' .. '9']
+nat = Nat . read <$> many1 digit
 
 
+parseExpr :: String -> Either ParseError Expr
+parseExpr = parse expr "* ParseError *"
 
--- parseTest expr "1+2"
+
+-- run expr "1+2"
 -- > Add (Nat 1) (Nat 2)
+run :: Show a => Parser a -> String -> IO ()
+run p input = case parse p "hcc" input of
+    Left  err -> putStr "parse error at" >> print err
+    Right x   -> print x
+
