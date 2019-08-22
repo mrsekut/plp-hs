66fa20659e5b13d6291c6beacf8540df580f5428
mrsekut <k.cloudspider@gmail.com>
2019-08-23 01:15:53 +0900

    [Add]use Identity Monad

--- src/others/codeGen/t.hs
+++ src/others/codeGen/t.hs
@@ -22,22 +22,27 @@ data Value = IntVal Integer         -- 整数
            deriving (Show)
 type Env = Map.Map Name Value
 
-eval0 :: Env -> Exp -> Value
-eval0 env (Lit i) = IntVal i
-eval0 env (Var n) = fromJust (Map.lookup n env)             -- Envの中から該当する変数名を探す。lookupはMaybeを返すのでfromJustで取り出す
-eval0 env (Plus e1 e2) =
-    let IntVal i1 = eval0 env e1
-        IntVal i2 = eval0 env e2
-    in  IntVal (i1 + i2)
-eval0 env (Abs n e) = FuncVal env n e
-eval0 env (App e1 e2) =
-    let val1 = eval0 env e1         -- FuncValのハズ
-        val2 = eval0 env e2         -- 引数
-    in  case val1 of
-            FuncVal env' n body -> eval0 (Map.insert n val2 env') body       -- 仮引数に実引数を入れて適用
+type Eval1 a = Identity a
+runEval1 :: Eval1 a -> a
+runEval1 ev = runIdentity ev
 
+eval1 :: Env -> Exp -> Eval1 Value
+eval1 env (Lit i) = return $ IntVal i
+eval1 env (Var n) =
+    -- lookupはどんなMonadのなかでもfail関数を呼べる
+    maybe (fail ("undeifned variable: " ++ n)) return $ Map.lookup n env                     -- Envの中から該当する変数名を探す。lookupはMaybeを返すのでfromJustで取り出す
+eval1 env (Plus e1 e2) = do
+    IntVal i1 <- eval1 env e1
+    IntVal i2 <- eval1 env e2
+    return $ IntVal (i1 + i2)
+eval1 env (Abs n  e ) = return $ FuncVal env n e
+eval1 env (App e1 e2) = do
+    val1 <- eval1 env e1         -- FuncValのハズ
+    val2 <- eval1 env e2         -- 引数
+    case val1 of
+        FuncVal env' n body -> eval1 (Map.insert n val2 env') body       -- 仮引数に実引数を入れて適用
 
 -- 12 + ((λx -> x)(4+2))
 exampleExp = Lit 12 `Plus` (App (Abs "x" (Var "x")) (Lit 4 `Plus` Lit 2))
--- > eval0 Map.empty exampleExp
+-- > runEval1 (eval1 Map.empty exampleExp)
 -- IntVal 18
