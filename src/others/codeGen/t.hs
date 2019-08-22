bd826157df0e2ee72dabee090ed08ecbf6de48d1
mrsekut <k.cloudspider@gmail.com>
2019-08-23 02:27:22 +0900

    [Add]ErrorT

--- src/others/codeGen/t.hs
+++ src/others/codeGen/t.hs
@@ -22,27 +22,37 @@ data Value = IntVal Integer         -- 整数
            deriving (Show)
 type Env = Map.Map Name Value
 
-type Eval1 a = Identity a
-runEval1 :: Eval1 a -> a
-runEval1 ev = runIdentity ev
 
-eval1 :: Env -> Exp -> Eval1 Value
-eval1 env (Lit i) = return $ IntVal i
-eval1 env (Var n) =
-    -- lookupはどんなMonadのなかでもfail関数を呼べる
-    maybe (fail ("undeifned variable: " ++ n)) return $ Map.lookup n env                     -- Envの中から該当する変数名を探す。lookupはMaybeを返すのでfromJustで取り出す
-eval1 env (Plus e1 e2) = do
-    IntVal i1 <- eval1 env e1
-    IntVal i2 <- eval1 env e2
-    return $ IntVal (i1 + i2)
-eval1 env (Abs n  e ) = return $ FuncVal env n e
-eval1 env (App e1 e2) = do
-    val1 <- eval1 env e1         -- FuncValのハズ
-    val2 <- eval1 env e2         -- 引数
+type Eval2 a = ErrorT String Identity a
+runEval2 :: Eval2 a -> Either String a          -- エラーメッセージと結果
+runEval2 ev = runIdentity (runErrorT ev)
+
+
+eval2 :: Env -> Exp -> Eval2 Value
+eval2 env (Lit i) = return $ IntVal i
+eval2 env (Var n) = case Map.lookup n env of
+    Nothing  -> throwError ("unbound variable: " ++ n)
+    Just val -> return val
+eval2 env (Plus e1 e2) = do
+    e1' <- eval2 env e1
+    e2' <- eval2 env e2
+    case (e1', e2') of
+        (IntVal i1, IntVal i2) -> return $ IntVal (i1 + i2)
+        _                      -> throwError "type error in additoin"
+eval2 env (Abs n  e ) = return $ FuncVal env n e
+eval2 env (App e1 e2) = do
+    val1 <- eval2 env e1         -- FuncValのハズ
+    val2 <- eval2 env e2         -- 引数
     case val1 of
-        FuncVal env' n body -> eval1 (Map.insert n val2 env') body       -- 仮引数に実引数を入れて適用
+        FuncVal env' n body -> eval2 (Map.insert n val2 env') body       -- 仮引数に実引数を入れて適用
+        _                   -> throwError "type error in application"
+
+
 
 -- 12 + ((λx -> x)(4+2))
 exampleExp = Lit 12 `Plus` (App (Abs "x" (Var "x")) (Lit 4 `Plus` Lit 2))
--- > runEval1 (eval1 Map.empty exampleExp)
--- IntVal 18
+
+-- > runEval2 (eval2 Map.empty (Plus (Lit 1) (Abs "x" (Var "x"))))
+-- Left "type error"
+-- > runEval2 (eval2 Map.empty (Var "x"))
+-- Left "undeifned variable: x"
