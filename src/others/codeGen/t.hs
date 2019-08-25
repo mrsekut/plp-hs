b5a787541976c0315b3d6277ffb4ee9d36608d8c
mrsekut <k.cloudspider@gmail.com>
2019-08-25 15:01:44 +0900

    [Add]IO

--- src/others/codeGen/t.hs
+++ src/others/codeGen/t.hs
@@ -23,41 +23,42 @@ data Value = IntVal Integer         -- 整数
 type Env = Map.Map Name Value
 
 
-type Eval5 a
-    = ReaderT Env (ErrorT String (WriterT [String] (StateT Integer Identity))) a
-runEval5 :: Env -> Integer -> Eval5 a -> ((Either String a, [String]), Integer)
-runEval5 env st ev =
-    runIdentity (runStateT (runWriterT (runErrorT (runReaderT ev env))) st)
+type Eval6 a
+    = ReaderT Env (ErrorT String (WriterT [String] (StateT Integer IO))) a
+runEval6
+    :: Env -> Integer -> Eval6 a -> IO ((Either String a, [String]), Integer)
+runEval6 env st ev = runStateT (runWriterT (runErrorT (runReaderT ev env))) st
 
-eval5 :: Exp -> Eval5 Value
-eval5 (Lit i) = do
+eval6 :: Exp -> Eval6 Value
+eval6 (Lit i) = do
     tick
+    liftIO $ print i
     return $ IntVal i
-eval5 (Var n) = do
+eval6 (Var n) = do
     tick
     tell [n]
     env <- ask
     case Map.lookup n env of
         Nothing  -> throwError ("unbound variable: " ++ n)
         Just val -> return val
-eval5 (Plus e1 e2) = do
+eval6 (Plus e1 e2) = do
     tick
-    e1' <- eval5 e1
-    e2' <- eval5 e2
+    e1' <- eval6 e1
+    e2' <- eval6 e2
     case (e1', e2') of
         (IntVal i1, IntVal i2) -> return $ IntVal (i1 + i2)
         _                      -> throwError "type error in additoin"
-eval5 (Abs n e) = do
+eval6 (Abs n e) = do
     tick
     env <- ask
     return $ FuncVal env n e
-eval5 (App e1 e2) = do
+eval6 (App e1 e2) = do
     tick
-    val1 <- eval5 e1         -- FuncValのハズ
-    val2 <- eval5 e2         -- 引数
+    val1 <- eval6 e1         -- FuncValのハズ
+    val2 <- eval6 e2         -- 引数
     case val1 of
         FuncVal env' n body ->
-            local (const (Map.insert n val2 env')) (eval5 body)       -- 仮引数に実引数を入れて適用
+            local (const (Map.insert n val2 env')) (eval6 body)       -- 仮引数に実引数を入れて適用
         _ -> throwError "type error in application"
 
 
@@ -68,11 +69,10 @@ tick = do
 
 -- 12 + ((λx -> x)(4+2))
 exampleExp = Lit 12 `Plus` (App (Abs "x" (Var "x")) (Lit 4 `Plus` Lit 2))
--- > runEval5 Map.empty 0 (eval5 exampleExp)
--- ((Right (IntVal 18),["x"]),8)
+-- > runEval6 Map.empty 0 (eval6 exampleExp)
+
 
 -- ((λx -> (λy -> x + y))(4)(2))
 otherExampleExp =
     App (App (Abs "x" (Abs "y" (Plus (Var "x") (Var "y")))) (Lit 4)) (Lit 2)
--- > runEval5 Map.empty 0 (eval5 otherExampleExp)
--- ((Right (IntVal 6),["x","y"]),9)
+-- > runEval6 Map.empty 0 (eval6 otherExampleExp)
