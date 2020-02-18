3f9a26b467dc018df0014cf29293823ed8514bba
mrsekut <k.cloudspider@gmail.com>
2020-02-18 23:22:35 +0900

    [Add]関数の呼び出し

--- src/eval/index.hs
+++ src/eval/index.hs
@@ -1,6 +1,7 @@
 -- STRefを使ったevalの練習
 import           Data.IORef
 import           Data.Maybe
+import           Control.Monad                  ( liftM )
 
 
 
@@ -9,6 +10,7 @@ data AST = Int Int
          | Var String
          | Assign String AST
          | Lambda String AST
+         | Call String AST
         deriving (Eq, Show)
 
 type Env = IORef [(String, IORef AST)]
@@ -30,7 +32,10 @@ eval (Assign var ast) env = do
 eval (Lambda f x) env = do
     envBind f x env
     eval (Int (-1)) env
-
+eval (Call f x) env = do
+    Lambda arg body <- getVar f env
+    localEnv        <- bindVars [(arg, x)] env
+    eval body localEnv
 
 
 {- 試す -}
@@ -47,13 +52,14 @@ useBindVar = do
     print =<< eval (Plus (Var "y") (Int 5)) env1 -- `y + 5` >> 13
 
 
--- 関数の定義
+-- 関数の定義と呼び出し
 useLambda :: IO ()
 useLambda = do
     env1 <- emptyEnv
     print =<< eval (Assign "error" (Int (-1))) env1
 
     print =<< eval (Assign "f" (Lambda "x" (Plus (Var "x") (Int 1)))) env1 -- `f = x => x + 1`
+    print =<< eval (Call "f" (Int 2)) env1 -- `f 2` >> 3
 
 main = useLambda
 
@@ -85,7 +91,6 @@ envBind var ast env = do
             e <- readIORef env
             writeIORef env ((var, a) : e)
             return ast
-
   where
     isBound :: String -> Env -> IO Bool
     isBound var env = isJust . lookup var <$> readIORef env
@@ -97,3 +102,15 @@ envBind var ast env = do
             Just v  -> writeIORef v ast
             Nothing -> return ()
         return ast
+
+
+bindVars :: [(String, AST)] -> Env -> IO Env
+bindVars bindings envRef = readIORef envRef >>= extendEnv bindings >>= newIORef
+  where
+    extendEnv :: [(String, AST)] -> [(String, IORef AST)] -> IO [(String, IORef AST)]
+    extendEnv bindings env = liftM (++ env) (mapM addBinding bindings)
+
+    addBinding :: (String, AST) -> IO (String, IORef AST)
+    addBinding (var, value) = do
+        ref <- newIORef value
+        return (var, ref)
